969282022f1a1ce13db60825046dc57ca9647780
Cristian Escalante <kykanbeca@gmail.com>
2023-06-20 18:08:09 -0300

    login

---
+++ src/DAO/models/users.model.js
@@ -0,0 +1,37 @@
+//@ts-check
+const { Schema, model }= require('mongoose');
+const monsoosePaginate= require('mongoose-paginate-v2');
+
+const schema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    max: 100,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    max: 100,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 100,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    max: 100,
+  },
+
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+schema.plugin(monsoosePaginate);
+const UserModel = model('users', schema);
+module.exports = {UserModel};
--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 
 const express= require("express");
+const session= require("express-session");
 const handlebars = require("express-handlebars");
 const path = require("path");
 const { Server } = require("socket.io");
+const cookieParser= require('cookie-parser')
+const MongoStore= require('connect-mongo')
 
 const productsRouter = require("./routes/products.router.js");
 const cartsRouter = require("./routes/cart.router.js");
@@ -13,7 +16,10 @@ const realTimeProductRouter = require("./routes/real.time.product.router.js");
 const ProductManager= require("./DAO/ProductManager.js");
 const {connectMongo, connectSocket} = require("./utils.js");
 const chatRouter = require("./routes/chats.router.js");
+const authRouter = require("./routes/auth.router.js");
 const productManager = new ProductManager();
+
+
 
 const app= express();
 const PORT= 8080;
@@ -28,15 +34,33 @@ app.engine("handlebars", handlebars.engine());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "handlebars");
 
+app.use(cookieParser());
+
+//session
+app.use(
+  session({
+    store: MongoStore.create({ mongoUrl: 'mongodb+srv://CristianEscalante:PmafqBcyCLDtFjEa@coderbackend.npm2s8y.mongodb.net/Ecommerce?retryWrites=true&w=majority', ttl: 7200 }),
+    secret: 'un-re-secreto',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+
 //API REST CON JSON
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
 //HTML RENDER
+app.get('/', (req, res) => {
+  return res.redirect('http://localhost:8080/auth/login');
+});
 app.use("/", fixedProductRouter);
 app.use("/realTimeProducts", realTimeProductRouter);
 app.use('/chat', chatRouter);
 app.use("/carts", cartRouter);
+app.use('/auth', authRouter);
+
 
 
 const httpServer=  app.listen(PORT, ()=>{
